178766bfaa2bcbf425ef07eb7c1b786b2379bf4b
Akash4056 <akashsaha1313@gmail.com>
2024-01-27 17:12:46 +0530

    main updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom'
+import { RouterProvider, createBrowserRouter} from 'react-router-dom'
 // import App from './App.jsx'
 import './index.css'
 import Login from './components/Login.jsx'
 import Signup from './components/Signup.jsx'
 import Layout from './Layout.jsx'
 
-// const router = createBrowserRouter(
-//   createRoutesFromElements(
-//       <Route path='/' element={<Layout/>} />
-//         <Route path='/' element={<Login/>} />
-//         <Route path='/signup' element={<Signup/>} />
 
-
-//   )
-// )
 
 const router = createBrowserRouter([
   {
