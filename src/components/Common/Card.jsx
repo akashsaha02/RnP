e2dff855356b215662754a32905ca453bf4386a6
Akash4056 <akashsaha1313@gmail.com>
2024-01-30 21:40:22 +0530

    employee card created

--- src/components/Common/Card.jsx
+++ src/components/Common/Card.jsx
@@ -1,6 +1,6 @@
 const Card = ({name, value, imgValue}) => {
     return (
-        <div className="flex items-center justify-around h-32 rounded bg-gray-50 dark:bg-gray-800">
+        <div className="flex items-center justify-around h-32 rounded bg-gray-50">
             <div className='ml-5'>
                 <h2 className=' font-bold text-xl'>{name}</h2>
                 <h2 className='font-extrabold text-3xl pt-2'>{value}</h2>
--- src/components/Dashboard/Projects.jsx
+++ src/components/Dashboard/Projects.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import Card from '../Common/Card'
+import ticketsOpen from '../../assets/ticketsOpen.png'
+import ticketsClosed from '../../assets/ticketsClosed.png'
 
 const Projects = () => {
   return (
     <div className="p-4 sm:ml-64">
       <div className="p-4 border-2 border-gray-200 rounded-lg dark:border-gray-700">
-      <h1>Projects</h1>
         <div className="grid grid-cols-3 gap-4 mb-4">
+          <Card name='Ongoing Projects' value='182' imgValue={ticketsOpen} />
+          <Card name='Closed Projects' value='321' imgValue={ticketsClosed} />
+          <Card name='Hold' value='48' imgValue={ticketsOpen} />
         </div>
         <div className="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800">
           <p className="text-2xl text-gray-400 dark:text-gray-500">
@@ -87,3 +92,5 @@ const Projects = () => {
 }
 
 export default Projects
+
+
