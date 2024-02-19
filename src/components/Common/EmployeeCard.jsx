8b0ee78b37c08fd8143045285a3bc21c2d186237
Akash4056 <akashsaha1313@gmail.com>
2024-02-19 15:46:17 +0530

    Employee Page Responsive for mobile screen

--- src/components/Common/EmployeeCard.jsx
+++ src/components/Common/EmployeeCard.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const EmployeeCard = ({ name, date, role, mobile, email , empImg}) => {
     return (
-        <div className="flex items-center justify-around rounded">
+        <div className="flex items-center justify-around rounded text-center">
             <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow">
-                <div className="flex flex-col items-center pb-5">
+                <div className="flex flex-col items-center px-2 pb-5">
                     <img className="w-24 h-24 mb-3 mt-3 rounded-full shadow-lg" src={empImg} alt="employee image" />
                     <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">{name}</h5>
                     <span className="text-sm text-gray-500 dark:text-gray-400">{date} - Present</span>
